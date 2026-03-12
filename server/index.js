a51d53057ae89568dd2b28f16c342bddba1e5f3e
Tannu Malik <maliktannu7178@gmail.com>
2026-03-12 17:12:36 +1300

    Changes to be committed:

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5173" || "https://onestep-healthcare-production.up.railway.app", credentials: true }));
 app.use(express.json());
 
 // ✅ Routes
