fc8ccb54da03e032107fa07b99f6036a9e97bccd
Tannu Tannu <167828617+TannuTannuMalik@users.noreply.github.com>
2026-03-12 14:41:24 +1300

    Fix socket URL port in socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL =
-  import.meta.env.VITE_SOCKET_URL || window.location.origin.replace(":5173", ":5000");
+  import.meta.env.VITE_SOCKET_URL || window.location.origin.replace(":5000");
 
 export const socket = io(SOCKET_URL, {
   transports: [ "polling", "websocket",],
